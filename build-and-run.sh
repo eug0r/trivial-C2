66dac74fc372d043aa3a10a06c78e16ec80076d8
paralispiss <felliotfzl@gmail.com>
2025-07-30 00:56:59 +0300

    first commit

--- build-and-run.sh
+++ build-and-run.sh
@@ -2,15 +2,10 @@
 
 set -e # Exit early if any commands fail
 
-# Copied from .codecrafters/compile.sh
-#
-# - Edit this to change how your program compiles locally
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
   cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=/opt/vcpkg/scripts/buildsystems/vcpkg.cmake
   cmake --build ./build
 )
 
-# Copied from .codecrafters/run.sh
-#
 exec $(dirname $0)/build/http-server "$@"
