770b49a117b0b9495a7502b6f32d9cbffd5e115c
Bardia Fazel <fz.bardia@proton.me>
2025-08-14 01:12:05 +0300

    add agent build only script

---
+++ agent-x64-linux/just-build.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e # Exit early if any commands fail
+
+(
+  cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
+  # cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=<PATH-TO-VCPKG>/scripts/buildsystems/vcpkg.cmake
+  cmake -B build -S . \
+    -DCMAKE_TOOLCHAIN_FILE=/home/$USER/.vcpkg/scripts/buildsystems/vcpkg.cmake \
+    -DVCPKG_TARGET_TRIPLET=x64-linux \
+  #
+  cmake --build ./build
+  cp ./build/c2-agent ./c2-agent
+)
